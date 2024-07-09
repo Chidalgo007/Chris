385b1671ccf15c79e97d2a5b42a2850b5d726e88
Christian Hidalgo <145306497+Chidalgo007@users.noreply.github.com>
2024-07-09 19:24:46 +1200

    Add files via upload

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,12 @@
 
 $(document).ready(function () {
 
+    $(".head-top").fadeIn(2000).css("display", "flex");
+
+    $(".intro").fadeIn(2000).css("display", "block");
     $(".icon.email").click(function () {
         $(".modal").slideDown(600);
-        $(".modal").css("display","flex");
+        $(".modal").css("display", "flex");
     });
 
     $(".close").click(function () {
@@ -11,10 +14,10 @@ $(document).ready(function () {
         $("form").get(0).reset();
     });
 
-    $(".btn-submit").click(function(){
+    $(".btn-submit").click(function () {
         $(".modal").hide(600);
-        $("form").get(0).reset();
+        $(".success").slideDown(800).delay(1000).slideUp(800);
     })
 
-})
+});
 
